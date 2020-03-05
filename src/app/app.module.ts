5379e090ace20dcfd2998481d124b8a54a96816c
MomenSherif <momensherif.2019@gmail.com>
2020-03-05 20:48:31 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './core/signup/signup.component';
+import { LoginComponent } from './core/login/login.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { CopyrightComponent } from './core/copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
