5e7fd47ef65c41f52c7d5004e0cbd3e47eb5783c
Menna <menna.elnokally@gmail.com>
2020-03-06 22:59:29 +0200

    Skills Added

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { InterestsComponent } from "./interests/interests.component";
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [InterestsComponent],
+  declarations: [InterestsComponent, SkillsComponent],
   imports: [CommonModule],
   exports: [InterestsComponent]
 })
