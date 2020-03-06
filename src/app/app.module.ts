31a44945a30fe28416f9a56725c537e51c34596c
Menna <menna.elnokally@gmail.com>
2020-03-06 16:33:35 +0200

    SignUpForm added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './core/signup/signup.component';
-import { LoginComponent } from './core/login/login.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { CopyrightComponent } from './core/copyright/copyright.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SignupComponent } from "./core/signup/signup.component";
+import { LoginComponent } from "./core/login/login.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { CopyrightComponent } from "./core/copyright/copyright.component";
 
 @NgModule({
   declarations: [
@@ -16,11 +17,8 @@ import { CopyrightComponent } from './core/copyright/copyright.component';
     FooterComponent,
     CopyrightComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
