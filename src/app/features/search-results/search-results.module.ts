369fd5f70e964771be9aa400c9845bfc8e953ffd
cadyemad1 <cady.emad@stud.fci-cu.edu.eg>
2020-03-07 20:06:56 +0200

    newsfeed and search results-ui

--- src/app/features/search-results/search-results.module.ts
+++ src/app/features/search-results/search-results.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchResultsComponent } from './search-results.component';
+import { ConnectListComponent } from './connect-list/connect-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchResultsComponent,
+    ConnectListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SearchResultsComponent,
+    ConnectListComponent
   ]
 })
 export class SearchResultsModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// news feed
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
