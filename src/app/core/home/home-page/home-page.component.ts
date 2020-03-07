877fe3bc60e9394dc1e3595323eda4d1e4945106
MomenSherif <momensherif.2019@gmail.com>
2020-03-07 11:43:06 +0200

    home page component added

---
+++ src/app/core/home/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
