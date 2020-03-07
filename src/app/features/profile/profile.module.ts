0db1639091fe77e4119083b853680c3bf4560f7e
Menna <menna.elnokally@gmail.com>
2020-03-07 11:28:30 +0200

    Accomplishments Added

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { InterestsComponent } from "./interests/interests.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { AccomplishmentsComponent } from "./accomplishments/accomplishments.component";
 
 @NgModule({
-  declarations: [InterestsComponent, SkillsComponent],
+  declarations: [InterestsComponent, SkillsComponent, AccomplishmentsComponent],
   imports: [CommonModule],
-  exports: [InterestsComponent, SkillsComponent]
+  exports: [InterestsComponent, SkillsComponent, AccomplishmentsComponent]
 })
 export class ProfileModule {}
