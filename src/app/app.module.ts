f5806fdf7bdb7059b143b65c4c3b1afd1daaafd6
MomenSherif <momensherif.2019@gmail.com>
2020-03-07 13:00:32 +0200

    signin form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileModule } from './features/profile/profile.module';
@@ -30,6 +30,7 @@ import { NavMainComponent } from './core/nav-main/nav-main.component';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     ProfileModule,
     HomeModule
   ],
--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  signInForm: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+    });
+
+  }
+  onSubmit() {
+    console.log(this.signInForm.value);
   }
 
 }
