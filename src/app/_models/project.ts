5d1bdacb6ab3020a5faf819f8d771e54715fc2e1
Menna <menna.elnokally@gmail.com>
2020-03-14 12:49:27 +0200

    Projects add and display

---
+++ src/app/_models/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  name?: string;
+  description?: string;
+  projectUrl?: string;
+  assciatedWith?: string;
+  startDate?: string;
+  endDate?: string;
+  currentlyWorkig?: boolean;
+}
--- src/app/features/profile/accomplishments/accomplishments.component.ts
+++ src/app/features/profile/accomplishments/accomplishments.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ProjectsService } from "src/app/_services/projects.service";
+import { Project } from "src/app/_models/project";
 
 @Component({
   selector: "app-accomplishments",
@@ -6,12 +8,22 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./accomplishments.component.scss"]
 })
 export class AccomplishmentsComponent implements OnInit {
-  editAccom: boolean[] = [false, false, false];
-  notEditAccom: boolean[] = [true, true, true];
+  editAccom: boolean[] = [];
+  notEditAccom: boolean[] = [];
   currentEditing: number = 0;
-  constructor() {}
+  addAccom: boolean = false;
+  projects: Project[] = [];
+  constructor(private projectService: ProjectsService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.projectService.getProjects().subscribe(snapshot => {
+      snapshot.docs.forEach(doc => {
+        this.projects.push(doc.data());
+        this.editAccom.push(false);
+        this.notEditAccom.push(true);
+      });
+    });
+  }
   onEditAccom() {
     this.editAccom[this.currentEditing] = true;
     this.notEditAccom[this.currentEditing] = false;
@@ -20,5 +32,24 @@ export class AccomplishmentsComponent implements OnInit {
   cancelEditAccom() {
     this.editAccom[this.currentEditing] = false;
     this.notEditAccom[this.currentEditing] = true;
+  }
+
+  onAddAccom() {
+    this.addAccom = true;
+  }
+
+  handelFormSubmission(project: Project) {
+    console.log(project);
+    this.projects.push(project);
+    this.editAccom.push(false);
+    this.notEditAccom.push(true);
+    this.projectService
+      .addProject(project)
+      .then(docRef => {
+        console.log("Document Added Succ ", docRef);
+      })
+      .catch(error => {
+        console.error("Error adding doc", error);
+      });
   }
 }
