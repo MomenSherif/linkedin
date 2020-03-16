e8ba7d51d406e5bb83f9d736def09291be57d510
cadyemad1 <cady.emad@stud.fci-cu.edu.eg>
2020-03-16 13:12:50 +0200

    ordered posts by date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
-import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAnalyticsModule } from "@angular/fire/analytics";
+import {
+  AngularFirestoreModule,
+  AngularFirestore
+} from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
 
-import { AppRoutingModule } from './app-routing.module';
-import { ProfileModule } from './features/profile/profile.module';
-import { HomeModule } from './core/home/home.module';
-import { SharedModule } from './shared/shared.module';
-import { NewsFeedModule } from './features/news-feed/news-feed.module';
-import { SearchResultsModule } from './features/search-results/search-results.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { ProfileModule } from "./features/profile/profile.module";
+import { HomeModule } from "./core/home/home.module";
+import { SharedModule } from "./shared/shared.module";
+import { NewsFeedModule } from "./features/news-feed/news-feed.module";
+import { SearchResultsModule } from "./features/search-results/search-results.module";
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './core/signup/signup.component';
-import { LoginComponent } from './core/login/login.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { CopyrightComponent } from './core/copyright/copyright.component';
-import { SearchPageComponent } from './features/search-results/search-page/search-page.component';
+import { AppComponent } from "./app.component";
+import { SignupComponent } from "./core/signup/signup.component";
+import { LoginComponent } from "./core/login/login.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { CopyrightComponent } from "./core/copyright/copyright.component";
+import { SearchPageComponent } from "./features/search-results/search-page/search-page.component";
 
-import { ExperienceSectionService } from './shared/experience-section/experience-section.service';
-
+import { ExperienceSectionService } from "./shared/experience-section/experience-section.service";
 
 @NgModule({
   declarations: [
@@ -31,7 +33,6 @@ import { ExperienceSectionService } from './shared/experience-section/experience
     FooterComponent,
     CopyrightComponent,
     SearchPageComponent
-
   ],
   imports: [
     BrowserModule,
@@ -48,10 +49,7 @@ import { ExperienceSectionService } from './shared/experience-section/experience
     HomeModule
     // AngularFireDatabaseModule
   ],
-  providers: [
-    AngularFirestore,
-    ExperienceSectionService
-  ],
+  providers: [AngularFirestore, ExperienceSectionService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
