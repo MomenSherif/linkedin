80fcfc61edfc71bb1684a7b390bf86a8175a2b62
Cady Emad <cady.emad@stud.fci-cu.edu.eg>
2020-03-16 21:03:27 +0200

    ordered posts by date-profile-cennections (#26)

--- src/app/features/news-feed/news-feed.component.ts
+++ src/app/features/news-feed/news-feed.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { PostService } from "src/app/_services/post.service";
 import { Observable } from "rxjs";
 import { Post } from "src/app/_models/post";
+import { User } from "src/app/_models/user";
+import { UsersService } from "src/app/_services/users.service";
 
 @Component({
   selector: "app-news-feed",
@@ -9,10 +11,31 @@ import { Post } from "src/app/_models/post";
   styleUrls: ["./news-feed.component.scss"]
 })
 export class NewsFeedComponent implements OnInit {
-  items: Observable<Post[]>;
-  constructor(private postService: PostService) {}
+  items: Post[];
+  users: User[];
+  connections: User[];
+  constructor(
+    private postService: PostService,
+    private userService: UsersService
+  ) {}
 
   ngOnInit(): void {
-    this.items = this.postService.getAllPosts();
+    this.postService.getAllPosts().subscribe(
+      data =>
+        (this.items = data.sort((a, b) => {
+          if (a.date < b.date) return 1;
+          else return -1;
+        }))
+    );
+
+    this.userService.getUsers().subscribe(data => {
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as User)
+        } as User;
+      });
+      this.connections = this.users.splice(1, 4);
+    });
   }
 }
