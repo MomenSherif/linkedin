cfbe64c19f6c41058a7aadb83d11f9ecce1a1bfd
MomenSherif <momensherif.2019@gmail.com>
2020-03-23 11:47:26 +0200

    Unsubscribe for observable @news-feed

--- src/app/features/news-feed/add-post/add-post.component.ts
+++ src/app/features/news-feed/add-post/add-post.component.ts
@@ -1,30 +1,32 @@
-import { UsersService } from "./../../../_services/users.service";
-import { PostService } from "src/app/_services/post.service";
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { formatDate } from "@angular/common";
-import { AuthService } from "src/app/_services/auth.service";
-import { User } from "src/app/_models/user";
+import { UsersService } from './../../../_services/users.service';
+import { PostService } from 'src/app/_services/post.service';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { AuthService } from 'src/app/_services/auth.service';
+import { User } from 'src/app/_models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-add-post",
-  templateUrl: "./add-post.component.html",
-  styleUrls: ["./add-post.component.scss"]
+  selector: 'app-add-post',
+  templateUrl: './add-post.component.html',
+  styleUrls: ['./add-post.component.scss']
 })
-export class AddPostComponent implements OnInit {
-  @ViewChild("textArea") txt;
+export class AddPostComponent implements OnInit, OnDestroy {
+  @ViewChild('textArea') txt;
   isPostingvalid = false;
   startPost = false;
   currentUser: string;
+  userSub: Subscription;
   user: User;
 
   constructor(
     private postService: PostService,
     private authService: AuthService,
     private userService: UsersService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    this.authService.user.subscribe(user => {
+    this.userSub = this.authService.user.subscribe(user => {
       this.currentUser = user.uid;
       this.userService.getUserById(this.currentUser).subscribe(data => {
         this.user = data.payload.data() as User;
@@ -33,22 +35,25 @@ export class AddPostComponent implements OnInit {
   }
 
   captionInput(e) {
-    e.target.value !== ""
+    e.target.value !== ''
       ? (this.isPostingvalid = true)
       : (this.isPostingvalid = false);
     // console.log("from caption input", e.target.value);
   }
 
   AddPost(value) {
-    let newPost = {
+    const newPost = {
       authorId: this.currentUser,
       caption: value,
-      date: formatDate(new Date(), "MMM d, y, h:mm:ss a", "en"),
+      date: formatDate(new Date(), 'MMM d, y, h:mm:ss a', 'en'),
       userLikes: []
     };
     this.postService.addPost(this.currentUser, newPost);
     this.startPost = false;
-    this.txt.nativeElement.value = "";
-    console.log("from addpost input", value);
+    this.txt.nativeElement.value = '';
+    console.log('from addpost input', value);
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe()
   }
 }
